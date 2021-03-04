f07b6af6a83b30b53a22a974ee14e20f92949f65
Krunal-97 <krunalparmar418@gmail.com>
2021-03-04 20:37:29 +0530

    connected to mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  "URI": "mongodb+srv://krunal:KfVQf30QKQP4tHke@cluster0.hmsb9.mongodb.net/books229?retryWrites=true&w=majority"
 };
